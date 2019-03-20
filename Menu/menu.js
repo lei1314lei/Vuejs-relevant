1f52e41711d3141804055b4032c7a4c088225594
martin.tian <547249121@qq.com>
2019-03-20 11:33:32 +0800

    no message

---
+++ Menu/menu.js
@@ -0,0 +1,35 @@
+var componentMenuItem={
+    props:['linkItem'],
+    data:function(){
+        return {
+            isActive:true,
+            cur_subitems:this.linkItem,
+        };
+    },
+    template:'<li class="item" :class=linkItem.class ><span class="dropdown-icon"></span><a :href=linkItem.link>{{linkItem.title}}</a><ul class="sub-menu"><li v-for="subitem in linkItem.subitems"  class="menu-item menu-item-type-custom menu-item-object-custom"><a target="_blank"  :href=subitem.link >{{subitem.title}}</a></li></ul></li>'
+}
+
+
+Vue.component('vue-menu',{
+    props:['menu_data'],
+    components:{'menu-item':componentMenuItem} ,
+    data:function(){
+        return this.menu_data;
+    },
+    render:function(createElement){
+        return createElement("ul",{ 'class':[this.class,"menu-container"],},this.subEles(createElement))
+    },
+    methods:{
+        subEles:function(createElement){
+                var eles=new Array();
+                for(var i in this.items)
+                {
+                  var ele=createElement('menu-item',{
+                      props:{linkItem:this.items[i]},
+                  });
+                  eles.push(ele);
+                }
+                return eles;
+        },  
+    },
+    })
